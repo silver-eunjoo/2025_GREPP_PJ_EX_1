a62d47d72248e9ea607630d2ef71b4539fa7200d
silver-eunjoo <leeeun5423@naver.com>
2025-04-22 10:20:44 +0900

    INIT : 프로젝트 세팅

---
+++ src/main/java/io/silver/exhandle/ExhandleApplication.java
@@ -0,0 +1,13 @@
+package io.silver.exhandle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExhandleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExhandleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/silver/exhandle/ExhandleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.silver.exhandle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExhandleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
